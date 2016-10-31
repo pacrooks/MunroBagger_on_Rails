adda95d60973eef16c4d4052638d3173d35e782d
johneas10 <easton3003@gmail.com>
2016-10-31 16:18:25 +0000

    added munro routes

---
+++ app/controllers/munro_controller.rb
@@ -0,0 +1,42 @@
+class MunroController < ApplicationController
+
+  # before_action(:authenticate_user!)
+
+  def shape_munro( mtn )
+    fc = Forecast.where( { munro_id: mtn.id } )
+    return {
+      id: mtn.id,
+      smcId: mtn.smcid,
+      name: mtn.name,
+      height: mtn.height,
+      gridRef: {
+        letters: mtn.gridref_letters,
+        eastings: mtn.gridref_eastings,
+        northings: mtn.gridref_northings
+      },
+      latLng: {
+        lat: mtn.latlng_lat,
+        lng: mtn.latlng_lng
+      },
+      region: mtn.region,
+      meaning: mtn.meaning,
+      weatherId: mtn.weatherid,
+      forecast: {
+        data: fc.first.data,
+        updated: fc.first.updated_at
+      }
+    }
+  end
+
+  def index
+    munros = Munro.all().map { | mtn | shape_munro(mtn) }
+
+    render({json: munros.as_json()})
+  end
+
+  def show
+    munro = shape_munro (Munro.find( params[:id] ).first)
+    render :json => munro
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
-    root 'welcome#index'
+
+  root 'welcome#index'
+
+  get 'munros' => "munro#index"
+  get 'munros/:id' => "munro#show"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
