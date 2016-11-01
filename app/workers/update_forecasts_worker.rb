066081db6ae4c2fa8f189588d80795340630d3f0
johneas10 <easton3003@gmail.com>
2016-11-01 10:26:31 +0000

    tidy up

--- app/workers/update_forecasts_worker.rb
+++ app/workers/update_forecasts_worker.rb
@@ -2,8 +2,7 @@ require 'httparty'
 
 class UpdateForecastsWorker
   include HTTParty
-  format :json
-
+  
   def self.get_forecast(weather_id, api_key)
     data = get("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/#{weather_id}?res=daily&key=#{api_key}")
     return data
@@ -16,7 +15,7 @@ class UpdateForecastsWorker
         puts "WeatherID = #{forecast.weatherid}"
         data = get_forecast(forecast.weatherid, key)
         puts "Weather data recieved"
-        json = data.to_json
+        json = data.fetch('SiteRep').fetch('DV')
         forecast.data = json
         forecast.save
         puts "Weather cached!"
@@ -25,15 +24,5 @@ class UpdateForecastsWorker
       sleep(1)
     end
   end
-
-  # def self.perform
-  #   Thread.new {
-  #     while true do
-  #       update_all_forecasts
-  #       puts("sleeping...")
-  #       sleep(4920)
-  #     end
-  #   }.join
-  # end
 
 end
